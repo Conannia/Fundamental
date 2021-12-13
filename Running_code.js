c0d958bec759511f48fdd422406e3eda305f87b7
Debu BerAkal <conannia17@gmail.com>
2021-12-14 06:31:25 +0700

    tm7

--- Running_code.js
+++
@@ -1,7 +0,0 @@
-
-console.log('Hello World')
-// comment itu adalah catatan atau tulisan yang tidak akan diexecute
-// comment untuk satu line cukup pakai double slash
-/* coment untuk lebih dari satu line
-pakai slash bintang lalu ditutup dengan bintang slash */
-// shortcut untuk comment adalah ctrl +'/'
