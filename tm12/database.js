20db948ef9467caf67d5fd95f56adbbe939c0d02
Conannia <95520277+Conannia@users.noreply.github.com>
2021-12-19 12:37:51 +0700

    Add files via upload

---
+++ tm12/database.js
@@ -0,0 +1,61 @@
+let products = [
+    {
+        id: 1639642627967,
+        kate: `Fast Food`,
+        nama: `Noodle`,
+        harga: 3500,
+        stok:9
+    },
+    {
+        id: 1639612327967,
+        kate: `Electronic`,
+        nama: `Headphone`,
+        harga: 350000,
+        stok: 8
+    },
+    {
+        id: 1639642626597,
+        kate: `Cloth`,
+        nama: `Hoodie`,
+        harga: 150000,
+        stok: 6
+    },
+    {
+        id: 1639642123967,
+        kate: `Fruit`,
+        nama: `Apple`,
+        harga: 13500,
+        stok: 7
+    }
+]
+
+class Product {
+    constructor(id, kate, nama, harga, stok){
+        this.id = id
+        this.kate = kate
+        this.nama = nama
+        this.harga = harga
+        this.stok = stok
+    }
+}
+
+let indexEdit = null
+
+let cart = [
+    // {
+    //     id: 1639642627967,
+    //     kate: `Fast Food`,
+    //     nama: `Noodle`,
+    //     harga: 3500,
+    //     qty: 9
+    // },
+    // {
+    //     id: 1639612327967,
+    //     kate: `Electronic`,
+    //     nama: `Headphone`,
+    //     harga: 350000,
+    //     qty: 8
+    // },
+]
+
+let totalPrice = 0
